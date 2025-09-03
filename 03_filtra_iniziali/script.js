48de0b26d04475b0839eae653b30d400839a42d4
AlessandroMessineo <alessandromessineo46@gmail.com>
2025-09-03 14:49:17 +0200

    completamento es filtra iniziali

--- 03_filtra_iniziali/script.js
+++ 03_filtra_iniziali/script.js
@@ -4,10 +4,29 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
 
 // Dichiara la funzione qui.
+const iniziali = (names, lettera) => {
 
+    // dichiarazione nuovo array
+    let arrIniziali = [];
+
+    // ciclo per scorrere l'array names
+    for (let i = 0; i < names.length; i++) {
+
+         // prende il nome corrente e lo salva
+        let saveNames = names[i];
+
+        // condizione per verificare che la prima lettera del nome sia uguale a quella inserita nella invocazione della funzione
+        if (saveNames[0] === lettera) {
+
+            // aggiunge il nome alla fine nell'array
+            arrIniziali.push(saveNames);
+        }
+    }
+    return arrIniziali;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+console.log(iniziali(names, "A"));
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
