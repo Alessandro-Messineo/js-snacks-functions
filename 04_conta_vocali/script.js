98d9fbc19aef6a4fc889c1eb146cb928ea34f3cf
AlessandroMessineo <alessandromessineo46@gmail.com>
2025-09-03 15:54:50 +0200

    completamento es conta vocali bonus

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -4,10 +4,28 @@ const word = 'javascript';
 
 
 // Dichiara la funzione qui.
+const contaVocali = (word) =>{
 
+    // dichiarazione contatore per le vocali
+    let count =0;
+    
+    // ciclo per scorrere le lettere della parola
+    for(let i=0; i< word.length; i++){
+        
+        // condizion per verificare che la lettera corrisponda a una vocale
+        if(word[i] === "a" || word[i] === "e" || word[i] === "i" || word[i] === "o" || word[i] === "u"){
+
+            // aggiornamento contatore per ogni vocale trovata
+            count++;
+        }
+    }
+    
+return count;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
+console.log(contaVocali(word));
 
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
